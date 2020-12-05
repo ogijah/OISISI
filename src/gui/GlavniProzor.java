4088c90459491dbf24977875ccaffd9bf84b323e
ogijah <bogdanovic.ra101.2018@uns.ac.rs>
2020-12-05 22:02:07 +0100

    #glavni_prozor

---
+++ src/gui/GlavniProzor.java
@@ -0,0 +1,39 @@
+package gui;
+
+
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+
+public class GlavniProzor extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7502172470744971352L;
+
+	public GlavniProzor() {
+		
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		Dimension ekran = kit.getScreenSize();
+		
+		//dimenzije ektrana
+		int visina = ekran.height;
+		int sirina = ekran.width;
+		
+		//postavljanje dimenzija prozora
+		setSize(sirina*3/4, visina*3/4);
+		
+		//naslov
+		setTitle("Studentska sluzba");
+		
+		//zatvaranje prozora
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//centriranje
+		setLocationRelativeTo(null);
+	}
+}
---
+++ src/gui/Main.java
@@ -0,0 +1,11 @@
+package gui;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GlavniProzor prozor = new GlavniProzor();
+		prozor.setVisible(true);
+	}
+
+}
